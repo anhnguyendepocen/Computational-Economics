f72ad5813c82257bf4f0aee8f8b1d8fe2489a8a4
yinpenghui <yinpenghui2008@gmail.com>
2016-03-20 12:34:38 +0100

    ...

--- Problem Set 6/ps_6.m
+++ Problem Set 6/ps_6.m
@@ -1,6 +1,7 @@
 clear
 close all
 clc
+global p alpha beta gamma sigma lam gamma_tild k g;
 
 
 %% Baseline Calibration
@@ -11,14 +12,16 @@ sigma=2.0;                  % coefficient of relative risk aversion
 lam=0.5;                    % second period productivity
 sig_eta=0.65;% standard deviation of idiosyncratic income shocks
 nk =50;
-k=linspace(0.3,0.45,nk)';    % number of grid points in capital grid
+k=linspace(0.31,0.5,nk)';    % number of grid points in capital grid
 % ns=10;
 % s=linspace(0.01,0.45,ns)';    % number of grid points in saving rate grid (pick maximum)
 tolf=1.0e-12;               % tolerance for function distance
 g=ones(nk,1);
-gnew=linspace(0.3,0.45,nk)';
+gnew=linspace(0.31,0.5,nk)';
 gamma_tild=1;
-
+phi1=(((1-alpha)*((1-alpha)/(1+lam))^(alpha/(1-alpha)))^(1-sigma))/(1-sigma);
+phi_tild=(((1+lam)/(1-alpha))^(alpha*(1-sigma)))/((1-alpha)^(1-sigma));
+A=(1-alpha)/(1+lam);
 %% Iteration on the first-order conditions
 options_fsolve = optimset('Display', 'off', 'TolX', 1e-12, 'TolFun', 1e-12, 'MaxIter', 3000, 'MaxFunEvals', 3000);
 while max(abs(gnew-g))>tolf
